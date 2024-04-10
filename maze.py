96c70867cfdfa5393cb11aae5fe0c8545f9ad4ce
arikru <krumbach.arian@gmail.com>
2024-04-10 23:12:09 +0200

    finished _create_cells method

--- maze.py
+++ maze.py
@@ -1,3 +1,5 @@
+from graphics import Cell
+
 class Maze:
     def __init__(
         self,
@@ -12,7 +14,7 @@ class Maze:
         self.x1 = x1
         self.y1 = y1
         self.num_rows = num_rows
-        self.num_rows = num_cols
+        self.num_cols = num_cols
         self.cell_size_x = cell_size_x
         self.cell_size_y = cell_size_y
         self.win = win
@@ -21,11 +23,24 @@ class Maze:
 
     def _create_cells(self):
         # fill a self._cells list with lists of cells. Each top-level list is a column of Cell objects; call its _draw_cell() method on each Cell
+        self._cells = []
+
+        for i in range(self.num_cols):
+            col_cells = []
+            for j in range(self.num_rows):
+                col_cells.append(Cell(self.win))
+            self._cells.append(col_cells)
+        
+        for i in range(self.num_cols):
+            for j in range(self.num_rows):
+                self._draw_cells(i, j)
+
 
     def _draw_cells(self, i, j):
         # This method should calculate the x/y position of the Cell based on i, j, the cell_size, and the x/y position of the Maze itself.
         # Once that's calculated, it should draw the cell and call the maze's _animate() method
+       pass 
 
     def _animate(self):
         # simply call the window's redraw() method, then sleep for a short amount of time, e.g. 0.05 seconds.
-
+        pass
